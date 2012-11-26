f7013408432f3b38aaa186d6344c0c5acc6da822
Chyld Medford <chyld.medford@gmail.com>
2012-11-26 15:38:31 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Barterville::Application.config.secret_token = '26657610be8476bc8069f034c69edf34263d36310eb4163c25b92e057e0dd5a00685613c0ba08aa7024048c6486d2023d66b53d528694249deda6da5695260e4'
